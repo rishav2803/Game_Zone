d1ddb95ffd18cf0dec71ec3b6744adfb7c1650e7
Rishav Rakesh Thapliyal <rishabhthapliyal03@gmail.com>
2022-12-28 17:43:23 +0530

    Removed unecessary files

--- Frontend/service/socket.ts
+++
@@ -1,6 +0,0 @@
-// export const newgame_socket=new WebSocket("ws://localhost:8080/newgame");
-// export const joingame_socket=new WebSocket("ws://localhost:8080/joingame");
-
-import io from "socket.io-client";
-
-export const socket = io.connect('http://localhost:4000')
